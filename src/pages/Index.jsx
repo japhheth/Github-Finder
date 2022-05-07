5872130dd94796acb5278fdbd3ea55d57ce838c0
Japheth Oyenuwe <japheth.oyenuwe@mypatricia.co>
2022-05-07 15:11:06 +0100

    initial-commit

---
+++ src/pages/Index.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/layout/Navbar";
+
+const Index = () => {
+  return (
+    <div className="h-screen flex flex-col justify-between">
+      <Navbar title="Github Finder" />
+    </div>
+  );
+};
+
+export default Index;
